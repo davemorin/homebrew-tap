9d315859591461893246e6b903f69fd19962e10e
Dave Morin <dave@morin.com>
2026-05-10 01:01:04 -0700

    Refresh supacrawl formula checksum

--- Formula/supacrawl.rb
+++ Formula/supacrawl.rb
@@ -2,7 +2,7 @@ class Supacrawl < Formula
   desc "Mirror Supabase/Postgres into local SQLite for search and offline analysis"
   homepage "https://github.com/davemorin/supacrawl"
   url "https://github.com/davemorin/supacrawl/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "52b65112cdc25f2887a8a570e1ddb645e1f108fd95b40b24875c9071e75640e7"
+  sha256 "400f6fd8d1a5f9495b34d5c5a04c4a64c2cc4aa3ea2e078816d038d3b24b1b80"
   license "MIT"
 
   depends_on "go" => :build
